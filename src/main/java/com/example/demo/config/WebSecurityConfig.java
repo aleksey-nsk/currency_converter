e3bc129c319a8a116f05e4de85971c2321c936d1
Alex Zhd <my_email@test.com>
2022-03-19 21:04:11 +0700

    вроде заработало на проде. Подправить, а то сейчас по-уродски

--- src/main/java/com/example/demo/config/WebSecurityConfig.java
+++ src/main/java/com/example/demo/config/WebSecurityConfig.java
@@ -1,36 +1,36 @@
-package com.example.demo.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
-
-@Configuration
-@Profile({"dev", "prod"})
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Bean
-    public UserDetailsService users() {
-        UserDetails userDetails = User.builder()
-                .username("user2")
-                .password("{bcrypt}$2a$10$4zyK6Oi2QWr/JMTheDTFduhQ9yY6AZ87Rzy5mNnRBQWosi//jMWYC") // пароль pswd2
-                .roles("USER")
-                .build();
-
-        return new InMemoryUserDetailsManager(userDetails);
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .anyRequest().authenticated() // на любой запрос требуем аутентификацию
-                .and().csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()) // для AngularJS
-                .and().formLogin(); // используем готовую форму логина от Спринга
-    }
-}
+//package com.example.demo.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.context.annotation.Profile;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.core.userdetails.User;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+//import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+//
+//@Configuration
+//@Profile({"dev", "prod"})
+//public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+//
+//    @Bean
+//    public UserDetailsService users() {
+//        UserDetails userDetails = User.builder()
+//                .username("user2")
+//                .password("{bcrypt}$2a$10$4zyK6Oi2QWr/JMTheDTFduhQ9yY6AZ87Rzy5mNnRBQWosi//jMWYC") // пароль pswd2
+//                .roles("USER")
+//                .build();
+//
+//        return new InMemoryUserDetailsManager(userDetails);
+//    }
+//
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.authorizeRequests()
+//                .anyRequest().authenticated() // на любой запрос требуем аутентификацию
+//                .and().csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()) // для AngularJS
+//                .and().formLogin(); // используем готовую форму логина от Спринга
+//    }
+//}
